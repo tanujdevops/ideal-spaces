b671b415bf9540fa96e2d15c848773e5f144e915
Tanuj <tanujsde@gmail.com>
2026-05-04 14:08:34 +0530

    Swap to dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
         <Providers>{children}</Providers>
       </body>
